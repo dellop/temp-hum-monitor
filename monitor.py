4e44167bdb0cf7b0f45e9cb7f6d8661c130b42ce
dellop <m@2kmark.com>
2018-01-30 15:21:42 -0600

    Cleaned up code, added comments

--- monitor.py
+++ monitor.py
@@ -11,8 +11,10 @@ import MySQLdb
 import RPi.GPIO as GPIO
 import ConfigParser
 
+# Pull config variables
 config = ConfigParser.ConfigParser()
-config.read('config.ini')
+configFile = os.getcwd()+'/config.ini'
+config.read(configFile)
 
 # Pin Definitions:
 humPin = int(config.get('Temp','humPin')) # Humidifier Relay
@@ -22,37 +24,39 @@ fanPin = int(config.get('Temp','fanPin')) # Fan Relay
 minHum = int(config.get('Temp','minHum'))
 maxHum = int(config.get('Temp','maxHum'))
 
+# GPIO Initial Settings
+GPIO.setwarnings(False) # Set GPIO to turn off warnings
+GPIO.setmode(GPIO.BCM) # Set GPIO identity mode to BCM
+GPIO.setup(humPin, GPIO.OUT) # Set the pin attached to the humidifier relay to out mode
+GPIO.setup(fanPin, GPIO.OUT) # Set the pin attached to the fan relay to out mode
 
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(humPin, GPIO.OUT)
-GPIO.setup(fanPin, GPIO.OUT)
-
+# Mysql Connect Strings
 db = MySQLdb.connect(config.get('Mysql','host'), config.get('Mysql','username'), config.get('Mysql','password'), config.get('Mysql','database'))
 curs = db.cursor()
 
-
+# Honeywell Website Configuration
 USERNAME = config.get('Honeywell','username')
 PASSWORD = config.get('Honeywell','password')
-
 LOGIN_URL = config.get('Honeywell','loginURL')
-URL = config.get('Honeywell','deviceURL')
+URL = config.get('Honeywell','deviceURL') # URL to actual thermostat device
 
-#Setup our API and delay
+#Setup Thingspeak API and delay
 myAPI = config.get('Thingspeak','apiKey')
 myDelay = 15 #how many seconds between posting data
 
+# Configure BME280 sensor
 sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 
-degrees = sensor.read_temperature()
-degrees = degrees*1.8+32
-pascals = sensor.read_pressure()
-pascals = pascals*0.00029529980164712
-humidity = sensor.read_humidity()
-res = int(os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline())
+degrees = sensor.read_temperature() # read temperature from sensor in Celcius
+degrees = degrees*1.8+32 # convert temperature to fahrenheit
+pascals = sensor.read_pressure() # read pressure from sensor in pascals
+pascals = pascals*0.00029529980164712 # convert pressure to inMg
+humidity = sensor.read_humidity() # read humidity from sensor
+res = int(os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline()) # get pi device temperature
 res = res/1000
-res = res*1.8+32
+res = res*1.8+32 # convert to fahrenheit
+
+# print for varification on command line
 print res
 print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
 print 'Temp      = {0:0.3f} deg F'.format(degrees)
